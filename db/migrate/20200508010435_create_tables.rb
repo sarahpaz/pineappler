a25f776c87df1f7ff9d5fa46917fec27d43470eb
Sarah Paz <sarah.paz@gmail.com>
2020-05-07 21:24:13 -0400

    #1 - define base schema

---
+++ db/migrate/20200508010435_create_tables.rb
@@ -0,0 +1,22 @@
+class CreateTables < ActiveRecord::Migration[6.0]
+  def change
+		create_table :goals, force: true do |t|
+			t.text :name
+			t.integer :user_id
+			t.timestamps
+		end
+		
+		create_table :tasks, force: true do |t|
+			t.text :name
+			t.integer :goal_id
+			t.timestamps
+		end
+
+		create_table :entity_tasks, force: true do |t|
+			t.datetime :completed_at
+			t.datetime :scheduled_at
+			t.integer :task_id
+			t.timestamps 
+		end
+  end
+end
